2c3de1efde008c737ff70e56e8fec5e62dab2fff
Chung Xa <wall85vn@yahoo.com>
2024-01-26 10:49:10 +0700

    Update example

--- wrap/node_list.lua
+++ wrap/node_list.lua
@@ -7,6 +7,9 @@ function NodeList.new(nodes)
 	local self = setmetatable({}, NodeList)
 	self._nodes = nodes
 	for key, node in pairs(self._nodes) do
+		if type(key) == "number" and type(node) == "string" then
+			key = node
+		end 
 		NodeList[key] = Node.new(node)
 	end
 	return self
